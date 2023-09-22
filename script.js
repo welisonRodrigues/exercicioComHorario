573fe409d72e0308ae9ce8ccc66aa0825a5141cc
welison <welisonrj@gmail.com>
2023-09-22 13:57:29 -0300

    horário da máquina no site

---
+++ script.js
@@ -0,0 +1,8 @@
+var horas = new Date()
+hora = horas.getHours()
+var minutos = new Date()
+min = minutos.getMinutes()
+
+var res = window.document.getElementById("res")
+res.innerHTML = hora +":" + min
+
